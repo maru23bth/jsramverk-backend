8ffd3147f119d5166dcd3df994981a5b110b4883
Martin <maru23@student.bth.se>
2024-09-17 18:18:09 +0200

    Add markdown rendering for README.md

--- app.mjs
+++ app.mjs
@@ -4,6 +4,9 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import documentsRoutes from './routes/documents.mjs';
+import showdown from 'showdown';
+import fs from 'fs';
+import path from 'path';
 
 
 const port = process.env.PORT;
@@ -13,11 +16,16 @@ app.disable('x-powered-by');
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 app.use('/documents', documentsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  const readmeContent = fs.readFileSync('README.md', 'utf8');
+  const converter = new showdown.Converter();
+  res.send(converter.makeHtml(readmeContent));
 });
