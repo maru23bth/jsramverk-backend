c669454a8ae70276f9119eee5c69e4e413595eb7
Olha Bruce <olbr22@student.bth.se>
2024-10-11 14:31:55 +0200

    Add server and client sockets; sending data and updating views

--- app.mjs
+++ app.mjs
@@ -6,10 +6,15 @@ import documentsRoutes from './routes/documents.mjs';
 import showdown from 'showdown';
 import fs from 'fs';
 
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
 
 const port = process.env.PORT || 1337;
 
 export const app = express();
+const httpServer = createServer(app);
+
 app.disable('x-powered-by');
 
 app.use(morgan('dev'));
@@ -18,7 +23,55 @@ app.use(cors());
 
 app.use('/documents', documentsRoutes);
 
-const server = app.listen(port, () => {
+const clientDev = 'http://localhost:3000';
+const clientProd = 'https://www.student.bth.se/~maru23/editor';
+const client = process.env.NODE_ENV === 'production' ? clientProd : clientDev;
+
+// console.log(client);
+// The Server instance io
+const io = new Server(httpServer, {
+  cors: {
+    origin: client
+  }
+});
+
+let timeout;
+
+// Handle Socket.IO events
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Connect to a room with id = documentId
+  socket.on('my-create-room', (documentId) => {
+    socket.join(documentId);
+    // console.log('rooms', socket.rooms);
+  });
+
+  // Listen on change of the content
+  socket.on('document-content-change', ({ documentId, content }) => {
+    // to the room with id
+    socket.to(documentId).emit('document-content-change', { documentId, content });
+    // throtling
+    clearTimeout(timeout);
+    timeout = setTimeout(async () => {
+      // update content to the database
+    }, 2000);
+
+  });
+
+  socket.on('document-title-change', ({ documentId, title }) => {
+    console.log('title', title);
+    // to the room with id
+    socket.to(documentId).emit('document-title-change', { documentId, title });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+
+const server = httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
