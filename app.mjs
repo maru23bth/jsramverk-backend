5b4cb39f0939e389d175c28030de596067d3ceff
Olha Bruce <olbr22@student.bth.se>
2024-10-22 12:09:40 +0200

    Add 'Authorization' cors

--- app.mjs
+++ app.mjs
@@ -29,7 +29,7 @@ const clientOlhaProd = 'https://www.student.bth.se/~olbr22/editor';
 const corsOptions = {
   origin: [clientDev, clientProd, clientOlhaProd],
   methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "Authentication", "x-email", "x-access-token"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-email", "x-access-token"],
   credentials: true
 };
 
